7dbd73f50714d9e6673fa0357d4c991dff3facbd
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-09-22 13:12:43 +0900

    :sparkles: SendMessageにchannelIDを追加

--- internal/client/webhook/client.go
+++ internal/client/webhook/client.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/ikura-hamu/q-cli/internal/client"
 )
 
@@ -37,7 +38,7 @@ func NewWebhookClient(webhookID string, hostName string, secret string) (*Webhoo
 	}, nil
 }
 
-func (c *WebhookClient) SendMessage(message string) error {
+func (c *WebhookClient) SendMessage(message string, channelID uuid.UUID) error {
 	if message == "" {
 		return client.ErrEmptyMessage
 	}
@@ -48,6 +49,10 @@ func (c *WebhookClient) SendMessage(message string) error {
 	}
 
 	req.Header.Set("Content-Type", "text/plain; charset=utf-8")
+
+	if channelID != uuid.Nil {
+		req.Header.Set("X-TRAQ-Channel-ID", channelID.String())
+	}
 
 	c.mac.Reset()
 	_, err = c.mac.Write([]byte(message))
