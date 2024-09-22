e619d514108ece14b4b71f8a253d95754c669221
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-09-22 23:47:28 +0900

    :wrench: バージョンごとにドキュメント生成するように

--- bin/docs/main.go
+++ bin/docs/main.go
@@ -1,14 +1,96 @@
 package main
 
 import (
+	"embed"
+	"flag"
+	"io/fs"
 	"log"
+	"os"
+	"path"
+	"regexp"
+	"text/template"
 
 	"github.com/ikura-hamu/q-cli/cmd"
 	"github.com/spf13/cobra/doc"
 )
 
+//go:embed templates
+var templateDir embed.FS
+
+const docsDir = "docs"
+
 func main() {
-	if err := doc.GenMarkdownTree(cmd.Command, "docs"); err != nil {
-		log.Fatalln(err)
+	var version string
+	flag.StringVar(&version, "version", "", "version of the CLI")
+
+	flag.Parse()
+
+	if version == "" {
+		log.Fatalf("version is required")
+	}
+	if !regexp.MustCompile(`^v\d+\.\d+\.\d+$`).MatchString(version) {
+		log.Fatalf("version must be in the format v1.2.3")
+	}
+
+	values := map[string]any{
+		"version": version,
+	}
+
+	versionDir := path.Join(docsDir, version)
+
+	err := os.MkdirAll(versionDir, 0755)
+	if err != nil {
+		log.Fatalf("failed to create version directory: %v", err)
+	}
+
+	generateFromCobra(versionDir)
+
+	entries, err := templateDir.ReadDir("templates")
+	if err != nil {
+		log.Fatalf("failed to read template dir: %v", err)
+	}
+
+	for _, entry := range entries {
+		generateFromTemplate(entry, versionDir, values)
+	}
+}
+
+func generateFromCobra(versionDir string) {
+	if err := doc.GenMarkdownTree(cmd.Command, docsDir); err != nil {
+		log.Fatalf("failed to gen cmd markdown: %v", err)
+	}
+	if err := doc.GenMarkdownTree(cmd.Command, versionDir); err != nil {
+		log.Fatalf("failed to gen cmd markdown: %v", err)
+	}
+}
+
+func generateFromTemplate(entry fs.DirEntry, versionDir string, values map[string]any) {
+	if entry.IsDir() {
+		return
+	}
+
+	fileName := entry.Name()
+
+	tmp, err := template.New(fileName).ParseFiles(path.Join("bin/docs/templates", fileName))
+	if err != nil {
+		log.Fatalf("failed to parse index template: %v", err)
+	}
+
+	outputFile, err := os.Create(path.Join(docsDir, fileName))
+	if err != nil {
+		log.Fatalf("failed to open index.md: %v", err)
+	}
+	defer outputFile.Close()
+	if err := tmp.Execute(outputFile, values); err != nil {
+		log.Fatalf("failed to execute template: %v", err)
+	}
+
+	outputVersionFile, err := os.Create(path.Join(versionDir, fileName))
+	if err != nil {
+		log.Fatalf("failed to open index.md: %v", err)
+	}
+	defer outputVersionFile.Close()
+	if err := tmp.Execute(outputVersionFile, values); err != nil {
+		log.Fatalf("failed to execute template: %v", err)
 	}
 }
---
+++ bin/docs/templates/index.md
@@ -0,0 +1,86 @@
+# `q-cli`
+
+`q-cli`は、[traQ](https://github.com/traQ)のWebhook機能を使ってtraQにテキストメッセージを送信するためのCLIツールです。
+
+**このドキュメントは{{ .version }}のものです。他のバージョンのドキュメントを確認したい場合は、GitHubの過去のリリースを確認してください。**
+
+## 各コマンド
+
+- [`q`](q.md)
+- [`q init`](q_init.md)
+
+## Preparation
+
+traQのWebhookをSecure形式で作成してください。
+traPで使用しているtraQインスタンスで使用する場合は、bot-consoleから作成できます。
+Webhook IDとWebhookシークレットが必要になります。
+
+## Installation
+
+1. [ダウンロード](#ダウンロード)
+2. [Webhookの設定](#webhookの設定)
+
+### ダウンロード
+
+#### Go
+
+```sh
+go install github.com/ikura-hamu/q-cli@{{ .version }}
+```
+
+`$GOPATH/bin`以下に`q-cli`という名前でインストールされます。
+
+#### GitHub Release
+
+リリースページから該当するOS、バージョンのものを探してダウンロードしてください。
+https://github.com/ikura-hamu/q-cli/releases
+
+```sh
+curl -OJL https://github.com/ikura-hamu/q-cli/releases/download/{{ .version }}/q-cli_{OS}_{architecture}.(tar.gz|zip)
+```
+
+ダウンロードしたら解凍します。
+
+```sh
+tar -zxvf q-cli_{OS}_{architecture}.tar.gz
+```
+
+や
+
+```sh
+unzip q-cli_{OS}_{architecture}.zip
+```
+
+など、適切な方法で解凍してください。
+
+解凍して出てくる`q`というファイルが実行ファイルです。パスが通る場所に移動させてください。`README.md`が一緒に解凍されますが、削除して問題ありません。
+
+### webhookの設定
+
+環境変数を用いるか、設定ファイルを配置します。
+
+#### 環境変数の場合
+
+`Q_WEBHOOK_HOST`: traQのドメイン
+`Q_WEBHOOK_ID`: traQのWebhook ID
+`Q_WEBHOOK_SECRET`: traQのWebhook シークレット
+
+#### 設定ファイルを使う場合
+
+以下のような`~/.q-cli.yml`を用意します。
+
+```json:.q-cli.yml
+webhook_host: "{traQのドメイン}"
+webhook_id: "{traQのWebhook ID}"
+webhook_secret: "{traQのWebhookシークレット}"
+channels:
+  channel: "{チャンネルのUUID}"
+```
+
+または、
+
+```sh
+q init
+```
+
+を実行することで、対話形式で設定を行えます。
