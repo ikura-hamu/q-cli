359501dc78a2f1bc38053294d035079c5d03a613
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2024-09-22 13:18:39 +0900

    :white_check_mark: テストでチャンネルIDを指定できるようにした

--- internal/client/webhook/client_test.go
+++ internal/client/webhook/client_test.go
@@ -6,51 +6,53 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/ikura-hamu/q-cli/internal/client"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSendMessage(t *testing.T) {
-	var (
-		mes  string
-		path string
-	)
-	ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		b, err := io.ReadAll(req.Body)
-		require.NoError(t, err)
-		mes = string(b)
-		path = req.URL.Path
-
-		res.WriteHeader(http.StatusNoContent)
-	}))
-	defer ts.Close()
-
-	webhookID := "test"
-	cl, err := NewWebhookClient(webhookID, ts.URL, "test")
-	require.NoError(t, err)
 
 	testCases := map[string]struct {
-		message string
-		isError bool
-		wantErr error
+		message   string
+		channelID uuid.UUID
+		isError   bool
+		wantErr   error
 	}{
-		"ok": {"test", false, nil},
-		"メッセージが空なのでエラー": {"", true, client.ErrEmptyMessage},
+		"ok": {"test", uuid.Nil, false, nil},
+		"メッセージが空なのでエラー": {"", uuid.Nil, true, client.ErrEmptyMessage},
 	}
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			// 共通のmesにリクエストボディを書き出しているので、t.Parallel()にはできない。
-			err := cl.SendMessage(tc.message)
+			t.Parallel()
+
+			webhookID := "test"
+
+			ts := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+				b, err := io.ReadAll(req.Body)
+				require.NoError(t, err)
+				mes := string(b)
+				path := req.URL.Path
+
+				assert.Equal(t, tc.message, mes)
+				assert.Equal(t, "/api/v3/webhooks/"+webhookID, path)
+
+				res.WriteHeader(http.StatusNoContent)
+			}))
+			defer ts.Close()
+
+			cl, err := NewWebhookClient(webhookID, ts.URL, "test")
+			require.NoError(t, err)
+
+			err = cl.SendMessage(tc.message, tc.channelID)
 
 			if tc.isError {
 				assert.ErrorIs(t, err, tc.wantErr)
 				return
 			}
 
-			assert.Equal(t, tc.message, mes)
-			assert.Equal(t, "/api/v3/webhooks/"+webhookID, path)
 		})
 	}
 }
